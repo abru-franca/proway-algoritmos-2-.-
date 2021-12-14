c00dc671daaf1ab5df20c0e302283401ac2c0f6c
Bruna de França Colla <70653@proway.treina>
2021-12-14 18:13:58 -0300

    subindo assuntos novo visual g code

---
+++ js/exercícioAula1Alg.js
@@ -0,0 +1,24 @@
+/*
+solicite uma quantidade definida de numero a serem capturados
+para cada captura deve verificar qual o maior numero e o menor numero 
+
+após a captura dos numeros exibir o maior o menor e a media geral desses numeros
+
+
+
+*/
+
+let nome="bruna de frança"
+
+
+let quantidade= number(prompt) ("informe uma quantidade de numeros (2) diferentes")
+
+for(let contador=1;contador<=quantidade;contador;cpntador++){
+
+let numero= number(prompt("informe um numero qualquer,mas qualquer mesmo"))
+let menor
+let maior
+
+if
+} 
+
